b72fddacd06deecab281f95e5bc09b5a72980288
oxygens <oxygens@yandex.ru>
2019-01-24 02:38:00 +0300

    refactoring for news json

--- VKServiceApp/VKServiceApp/Models/News/News.swift
+++ VKServiceApp/VKServiceApp/Models/News/News.swift
@@ -23,24 +23,15 @@ class News {
     
     convenience init(json: JSON) {
         self.init()
-        self.title = json["text"].stringValue
-        let maxPhoto = json["attachments"][0]["photo"]["sizes"].arrayValue.count
-        if maxPhoto > 0 {
-            self.image = json["attachments"][0]["photo"]["sizes"][maxPhoto-1]["url"].stringValue
-        } else {
-            self.image = json["copy_history"]["attachments"][0]["photo"]["sizes"][0]["url"].stringValue            
-        }
         
+        self.title = getPostTitle(from: json)
+        self.image = getPostImageUrl(from: json)
         self.comments = []
-        if (json["likes"]["user_likes"].intValue == 0) {
-            self.myLike = false
-        } else {
-            self.myLike = true
-        }
-        self.likesCount = json["likes"]["count"].intValue
-        self.commentsCount = json["comments"]["count"].intValue
-        self.viewsCount = json["views"]["count"].intValue
-        self.ownerId = json["attachments"][0]["photo"]["owner_id"].intValue
+        self.myLike = getPostMyLike(from: json)
+        self.likesCount = getPostLikesCount(from: json)
+        self.commentsCount = getPostCommentsCount(from: json)
+        self.viewsCount = getPostViewCount(from: json)
+        self.ownerId = getPostOwnerId(from: json)
     }
     
     convenience init(title: String, image: String, comments: [String], myLike: Bool, commentsCount: Int, likesCount: Int, viewsCount: Int, ownerId: Int){
@@ -53,6 +44,60 @@ class News {
         self.commentsCount = commentsCount
         self.viewsCount = viewsCount
         self.ownerId = ownerId
+    }
+
+    
+    func getPostLikesCount(from json: JSON) -> Int {
+        return json["likes"]["count"].intValue
+    }
+    
+    func getPostCommentsCount(from json: JSON) -> Int {
+        return json["comments"]["count"].intValue
+    }
+    
+    func getPostViewCount(from json: JSON) -> Int {
+        return json["views"]["count"].intValue
+    }
+    
+    func getPostOwnerId(from json: JSON) -> Int {
+        if (json["copy_history"].arrayValue.count > 0){
+            return json["copy_history"][0]["owner_id"].intValue
+        }
+        return json["attachments"][0]["photo"]["owner_id"].intValue
+    }
+    
+    func getPostMyLike(from json: JSON) -> Bool {
+        if !(json["likes"]["user_likes"].intValue == 0) {
+            return true
+        }
+        return false
+    }
+    
+    func getPostTitle(from json: JSON) -> String {
+        if (json["copy_history"].arrayValue.count > 0){
+            return json["copy_history"][0]["text"].stringValue
+        }
+        return json["text"].stringValue
+    }
+    
+    func getPostImageUrl(from json: JSON) -> String {
+        if (json["copy_history"].arrayValue.count > 0) {
+            if (json["copy_history"][0]["attachments"][0]["type"].stringValue == "album"){
+                return json["copy_history"][0]["attachments"][0]["album"]["thumb"]["sizes"][0]["url"].stringValue
+            } else {
+                return json["copy_history"][0]["attachments"][0]["photo"]["sizes"][0]["url"].stringValue
+            }
+        } else {
+            if (json["attachments"][0]["type"].stringValue == "album"){
+                return json["attachments"][0]["album"]["photo"]["sizes"][0]["url"].stringValue
+            } else {
+                let maxPhoto = json["attachments"][0]["photo"]["sizes"].arrayValue.count
+                if maxPhoto > 0 {
+                    return json["attachments"][0]["photo"]["sizes"][maxPhoto-1]["url"].stringValue
+                }
+            }
+        }
+        return ""
     }
     
     func getTitle() -> String{
